bec4310e3405521de5afbd8399cb0d267a09b1d8
Asger124 <asjo@itu.dk>
2025-03-27 16:07:38 +0100

    Most features work on mydoctors - remember to figure out,
    what do with the checkstate, updating the rows arr before reload

--- frontend/app/pages/mydoctors.tsx
+++ frontend/app/pages/mydoctors.tsx
@@ -19,14 +19,17 @@ enum DoctorTypes{
   KOne = "K1",
   Intro = "Intro",
   ResTemp  = "Res.vik",
-  None = "Sæt type her"
 }
 
 interface Row {
   id: number;
   name: string;
+  tag: string;
+  Initials:string;
   email: string;
   type:DoctorTypes; 
+  capacity: number;
+  Status:string;
 }
 
 export default function EditableTable() {
@@ -50,6 +53,10 @@ export default function EditableTable() {
     };
     return [];
   });
+
+  const [editingId, setEditingId] = useState<number | null>(null);
+
+  const[originalRow, setOrignalRow] = useState<Row| null>(null); 
    
   //Runs when rows changes. Store the array rows in localstorage
   //ensures that data persists across page reloads 
@@ -61,30 +68,83 @@ export default function EditableTable() {
       }
     }, [rows]);
 
+    useEffect(() => {
+      const beforeUnload = (event: BeforeUnloadEvent) => {
+  
+        if (editingId!=null) {
+          const confirmation = window.confirm("You have unsaved changes, do you really want to leave?");
+  
+          //Confirmation returns bool based on user interaction. If user clicks "Genindlæs/ok" then it is set to true,
+          //otherwise false. If it evaluates to false we prevent a reload and stay on page.
+          if (!confirmation) {
+            event.preventDefault(); 
+          }
+        }
+    };
+    window.addEventListener("beforeunload", beforeUnload);
+      
+    return () => {
+      window.removeEventListener("beforeunload", beforeUnload);
+    };
+  }, [editingId]);
 
-  //Editing id can either be null or a number
-  const [editingId, setEditingId] = useState<number | null>(null);
+
+  const handleCheck = (id:number, checked:boolean) => {
+    setRows(rows.map(row => row.id === id ? { ...row, Status : checked? "Active" : "Inactive" } : row));
+    
+
+  }
 
   const handleAddRow = () => {
-    const newRow: Row = { id: Date.now(), name: "", email: "", type: DoctorTypes.None };
+    const newRow: Row = { id: Date.now(), name: "", Initials:"", tag:"",  email: "", type: DoctorTypes.ResTemp, capacity: 0, Status : "Inactive"};
     setRows([...rows, newRow]);
     setEditingId(newRow.id);
   };
 
   //Match this with a row.id if editing
-  const handleEdit = (id: number) => {
+  const handleEdit = (id: number, name: string, init:string, email: string, type:DoctorTypes, capacity:number,stat:string) => {
+   
+    if(editingId!= null && editingId !==id) {
+    const confirm = window.confirm(`Hov! Du er i gang med at redigere en lægetype. Vil du gemme dine ændringer?`);
+
+    if(confirm){
+    handleSave(id,name,init,email,type,capacity,stat);
+    } else {
+      handleAbort(editingId) 
+    }
+  }
+    const rowToEdit = rows.find((row) => row.id === id);
+    if (rowToEdit) {
+      setOrignalRow({...rowToEdit}); // Save the original type value
+    }
     setEditingId(id);
+    };
+  
+  //create a new row object and set editing id to null
+  const handleSave = (id: number, name: string, init:string, email: string, type:DoctorTypes, capacity:number,stat:string) => {
+    setRows(rows.map(row => row.id === id ? { ...row, name, init, email, type, capacity, stat } : row));
+    setEditingId(null);
+    setOrignalRow(null);
   };
 
-  //create a new row object and set editing id to null
-  const handleSave = (id: number, name: string, email: string, type:DoctorTypes) => {
-    setRows(rows.map(row => row.id === id ? { ...row, name, email, type } : row));
-    setEditingId(null);
+  const handleAbort = (id:number) => {
+    if (originalRow) {
+      // Revert to the original row state
+      setRows(rows.map(row => row.id === id ? originalRow : row));
+    }
+    setEditingId(null); // Stop editing
+    setOrignalRow(null); // Clear the original row after abort
   };
+
+
   // return rows without specific entry(id) in it 
-  const handleDelete = (id: number) => {
-    setRows(rows.filter(row => row.id !== id));
-  };
+  const handleDelete = (id: number, name:string) => {
+    const confirmed = window.confirm(`Er du sikker på at du vil slette "${name}"?`);
+      if (confirmed) {
+      setRows(rows.filter(row => row.id !== id));
+      }
+      setEditingId(null);
+    };
   
 
   return (
@@ -93,13 +153,16 @@ export default function EditableTable() {
         Velkommen til siden 'Vores læger' <br /> 
         her kan du tilføje, redigere og slette oplysninger om læger
       </h1>
-      <div className=" w-full max-w-3xl bg-white shadow-lg rounded-lg p-6 overflow-auto max-h-[80vh] mb-30">
+      <div className=" w-full max-w-6xl bg-white shadow-lg rounded-lg p-6 overflow-auto max-h-[80vh] mb-30">
       <Table>
         <TableHeader>
           <TableRow>
             <TableHead>Navn</TableHead>
+            <TableHead>Initialer</TableHead>
             <TableHead>Email</TableHead>
             <TableHead>Type</TableHead>
+            <TableHead>Kapacitet</TableHead>
+            <TableHead>Status</TableHead>
             <TableHead>Rediger/Slet</TableHead>
           </TableRow>
         </TableHeader>
@@ -114,11 +177,23 @@ export default function EditableTable() {
                   row.name
                 )}
               </TableCell>
-
               <TableCell>
                 {editingId === row.id ? (
+                  <Input className="w-24" defaultValue={row.Initials} onChange={(e) => row.Initials = e.target.value} />
+                ) : (
+                  row.Initials
+                )}
+              </TableCell>
+
+              <TableCell>
+ 
+                {editingId === row.id ? (
                   <Input
-                  type = "email" defaultValue = {row.email} onChange={(e) => row.email = e.target.value} />
+                  type = "email" pattern=".+@region\.com" 
+                  title="Mailen skal indeholde @region.com"
+                  defaultValue = {row.email} 
+                  onChange={(e) =>{ row.email = e.target.value; 
+                    const emailInput = e.target; emailInput.reportValidity();}} />
                 ) : (
                     row.email
                 )}
@@ -141,13 +216,29 @@ export default function EditableTable() {
 
               <TableCell>
                 {editingId === row.id ? (
-                  <Button className="hover:cursor-pointer"size="sm" onClick={() => handleSave(row.id, row.name, row.email, row.type)}>
+                  <Input className ="w-20"type = "number" defaultValue={row.capacity} onChange={(e) => row.capacity = e.target.valueAsNumber} />
+                ) : (
+                    row.capacity
+                )}
+              </TableCell>
+
+              <TableCell>
+                {editingId === row.id ? (
+                  <Input className="w-10" type ="checkbox" checked={row.Status ==="Active"} onChange={(e) => handleCheck(row.id,e.target.checked)} />
+                ) : (
+                    row.Status
+                )}
+              </TableCell>
+              
+              <TableCell>
+                {editingId === row.id ? (
+                  <Button className="hover:cursor-pointer"size="sm" onClick={() => handleSave(row.id, row.name,row.Initials, row.email, row.type, row.capacity,row.Status)}>
                     <SaveIcon /></Button>
                 ) : (
                   <>
-                    <Button className="hover:cursor-pointer" variant= "secondary"  onClick={() => handleEdit(row.id)}>
+                    <Button className="hover:cursor-pointer" variant= "secondary"  onClick={() => handleEdit(row.id,row.name,row.Initials, row.email, row.type, row.capacity,row.Status)}>
                       <EditIcon /></Button>
-                    <Button className="ml-2 hover:cursor-pointer" variant="destructive" size="sm" onClick={() => handleDelete(row.id)}>
+                    <Button className="ml-2 hover:cursor-pointer" variant="destructive" size="sm" onClick={() => handleDelete(row.id,row.name)}>
                       <Trash2Icon /></Button>
                   </>
                 )}
